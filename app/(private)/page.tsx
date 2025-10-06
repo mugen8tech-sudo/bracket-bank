6b1834fecdd0d534993bbb484e0ea4dc024b609b
mugenzen <mugen8.tech@gmail.com>
2025-10-06 17:44:07 +0700

    Update page.tsx

--- app/(private)/page.tsx
+++ app/(private)/page.tsx
@@ -1,7 +1,7 @@
 export default function Landing() {
   return (
     <div className="rounded border bg-white p-6">
-      <h1 className="text-xl font-semibold mb-2">Selamat datang di Bracket BANK</h1>
+      <h1 className="text-xl font-semibold mb-2">Login Sukses.</h1>
       <p className="text-sm text-gray-600">
         Tenant aktif: <b>TECH</b>. Gunakan menu untuk mengelola data.
       </p>
