68318bb63ab3033242906850f7c1ffbd1f7a3b1f
mugenzen <mugen8.tech@gmail.com>
2025-10-06 16:31:37 +0700

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>Bracket BANK</h1>
+      <p>Init commit berhasil. Lanjut tambahkan kode fitur.</p>
+    </main>
+  );
+}
