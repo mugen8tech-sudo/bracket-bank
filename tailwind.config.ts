1bcc522e1e8e75917cb996fb0e9a0117ee4f8bd4
mugenzen <mugen8.tech@gmail.com>
2025-10-06 17:19:53 +0700

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,10 +3,8 @@ import type { Config } from "tailwindcss";
 export default {
   content: [
     "./app/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}"
   ],
-  theme: {
-    extend: {},
-  },
+  theme: { extend: {} },
   plugins: [],
 } satisfies Config;
