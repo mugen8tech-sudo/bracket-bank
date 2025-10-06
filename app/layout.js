aad5faff5b64180a8f0db25ef7f11f845097c2fc
mugenzen <mugen8.tech@gmail.com>
2025-10-06 16:31:22 +0700

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,9 @@
+export const metadata = { title: 'Bracket BANK' };
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="id">
+      <body>{children}</body>
+    </html>
+  );
+}
