7f27b1d1fbc72cb30d2e0f3fa891e42aae83b1bf
mugenzen <mugen8.tech@gmail.com>
2025-10-06 17:15:40 +0700

    Delete app/page.js

--- app/page.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: 24 }}>
-      <h1>Bracket BANK</h1>
-      <p>Init commit berhasil. Lanjut tambahkan kode fitur.</p>
-    </main>
-  );
-}
