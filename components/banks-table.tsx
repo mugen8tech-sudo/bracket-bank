01f53cbf953d53600b9c7ca9c48d7fe76e7d062e
mugenzen <mugen8.tech@gmail.com>
2025-10-08 20:36:52 +0700

    Update banks-table.tsx

--- components/banks-table.tsx
+++ components/banks-table.tsx
@@ -73,9 +73,7 @@ function toNumber(input: string) {
 function nowLocalDatetimeValue() {
   const d = new Date();
   const pad = (n: number) => n.toString().padStart(2, "0");
-  return `${d.getFullYear()}-${pad(d.getMonth() + 1)}-${pad(
-    d.getDate()
-  )}T${pad(d.getHours())}:${pad(d.getMinutes())}`;
+  return `${d.getFullYear()}-${pad(d.getMonth() + 1)}-${pad(d.getDate())}T${pad(d.getHours())}:${pad(d.getMinutes())}:${pad(d.getSeconds())}`;
 }
 
 // === Helpers (signed) untuk Adjustment ===
