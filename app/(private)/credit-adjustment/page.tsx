7f9e42590949fc8377bab4210bbdf33d22a2e0a3
mugenzen <mugen8.tech@gmail.com>
2025-10-11 13:44:04 +0700

    Create page.tsx

---
+++ app/(private)/credit-adjustment/page.tsx
@@ -0,0 +1,7 @@
+import CreditAdjustment from "@/components/credit-adjustment";
+
+export const metadata = { title: "Credit Adjustment" };
+
+export default function Page() {
+  return <CreditAdjustment />;
+}
